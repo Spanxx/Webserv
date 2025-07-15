3fd240b6ff862f866fcf1bc3aa5085ff309da913
hzimmerm <hzimmerm@student.42berlin.de>
2025-07-15 13:16:28 +0200

    adjust hello.php

--- www/BackupServer/cgi-bin/hello.php
+++ www/BackupServer/cgi-bin/hello.php
@@ -7,5 +7,5 @@ $name = $_GET["name"] ?? null;
 $name = isset($name) && $name !== "" ? htmlspecialchars($name) : "stranger";
 
 echo "\n";
-echo "<html><body><h1>Hello, $name!</h1><br></br><form action=\"/index.html\" method=\"get\"><button type=\"submit\">Go Back</button></form></body></html>";
+echo "<html><body><h1>Hello, $name!</h1><br></br><form action=\"/about.html\" method=\"get\"><button type=\"submit\">Go Back</button></form></body></html>";
 ?>
